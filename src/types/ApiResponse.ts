663f029895fe374484c86c88fed1884c01734d5e
SidhantR <sidhantrc02@gmail.com>
2024-05-18 18:27:01 +0530

    Revert "signIn Complete"
    This reverts commit 8ee443c2501cadcfc9352d975b402abe6f5049ec.

--- src/types/ApiResponse.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "@/model/User";
-
-export interface ApiResponse {
-    success: boolean;
-    message: string;
-    isAcceptingMessagr?: boolean
-    messages?: Array<Message>
-}
