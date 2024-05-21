fd0b518dd70839863b4d94285648d8ef0ef476ea
SidhantR <sidhantrc02@gmail.com>
2024-05-21 16:05:56 +0530

    Verify Code functionality

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,50 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request){
+    await dbConnect()
+    try {
+        const {username, code} = await request.json()
+
+        const user = await UserModel.findOne({username: username })
+
+        if(!user){
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, {status: 500})
+        }
+
+        const isCodeValid = user.verifyCode === code
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+        if(isCodeValid && isCodeNotExpired){
+            user.isVerified = true
+            await user.save()
+            
+            return Response.json({
+                success: true,
+                message: "Account verified successfully "
+            }, {status:200})
+
+        } else if(!isCodeNotExpired) {
+            return Response.json({
+                success: false,
+                message: "Verification Code has expire. please login again to get a new code"
+            }, {status: 400})
+
+        } else {
+            return Response.json({
+                success: false,
+                message: "Incorrect Verification Code"
+            }, {status: 400})
+        }
+
+    } catch(err){
+        console.error("Error verifying user", err)
+        return Response.json({
+            success: false,
+            message: 'Error verifying user'
+        }, {status: 500})
+    }
+}
